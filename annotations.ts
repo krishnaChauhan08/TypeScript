0ce0b9144c8f2165e92824d8ac4ce6f62d05042e
krishnaChauhan08 <chauhankrishna0820@gmail.com>
2025-02-20 11:01:30 +0530

    annotation

---
+++ annotations.ts
@@ -0,0 +1,14 @@
+// Strings
+let myName: string = "Krishna Chauhan";
+console.log(myName);
+myName = "Sanu Chauhna";
+console.log(myName);
+
+// Numbers
+let favNumber: number = 20;
+console.log(favNumber);
+
+// Booleans
+let isTypescriptHard: boolean = false;
+isTypescriptHard = true;
+console.log(isTypescriptHard);
