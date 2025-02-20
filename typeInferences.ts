1ccdbefe90ab2b48f1af32a13f08fae7d08d55e5
krishnaChauhan08 <chauhankrishna0820@gmail.com>
2025-02-20 11:12:49 +0530

    inferences in ts

---
+++ typeInferences.ts
@@ -0,0 +1,11 @@
+let tech = "typeScript";
+console.log(tech);
+console.log(typeof tech);
+
+let favNum = 20;
+console.log(favNum);
+console.log(typeof favNum);
+
+let tshard = true;
+console.log(tshard);
+console.log(typeof tshard);
