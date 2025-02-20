53e3dfd7ceecd65f876da0d44c6976203cacb8d2
krishnaChauhan08 <chauhankrishna0820@gmail.com>
2025-02-20 21:21:13 +0530

    arrays in ts

---
+++ arrayTypes.ts
@@ -0,0 +1,22 @@
+// using bracket notation
+
+const nums: number[] = [1, 2, 3];
+console.log(nums);
+const str: string[] = ["one", "two", "three"];
+console.log(str);
+
+const items: string[] = [];
+items.push("keyword");
+// items.push(10) --> give error
+console.log(items);
+
+const numList: number[] = [];
+numList.push(20);
+console.log(numList);
+
+// using Array<type> notation
+
+const item: Array<string> = [];
+item.push("krishna");
+console.log(item);
+
---
+++ multidimensionalArray.ts
@@ -0,0 +1,7 @@
+const singledi: number[] = [1, 2, 3, 4];
+const multidi: number[][] = [[1, 2, 3, 3]];
+const triple: number[][][] = [[[1, 2, 3, 4, 4]]];
+
+console.log(singledi);
+console.log(multidi);
+console.log(triple);
